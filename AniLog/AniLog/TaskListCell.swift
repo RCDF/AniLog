b12b40adb5560364a5a3d6a598f62d7c6a796544
David Fang <i.davidfang@gmail.com>
2017-03-20 22:58:26 -0700

    Add tasks list to project
    (1) Disable UITextField warnings
    (2) Create TaskListVC and cells
    
    To-do: Requires UITextField delegates and functionality

---
+++ AniLog/AniLog/TaskListCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskListCell.swift
+//  AniLog
+//
+//  Created by David Fang on 3/20/17.
+//  Copyright © 2017 RCDF. All rights reserved.
+//
+
+import UIKit
+
+class TaskListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AniLog/AniLog/TaskListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TaskListViewController.swift
+//  AniLog
+//
+//  Created by David Fang on 3/20/17.
+//  Copyright © 2017 RCDF. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskListCell", for: indexPath) as! TaskListCell
+        
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsets.zero
+        cell.layoutMargins = UIEdgeInsets.zero
+        
+        return cell
+    }
+}
