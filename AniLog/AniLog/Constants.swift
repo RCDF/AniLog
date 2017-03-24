66620849b8478a5ce42778f105d21c5e1af2428b
David Fang <i.davidfang@gmail.com>
2017-03-24 11:32:05 -0700

    Fix sidemenu black status bar issue
    (1) Created Constants file
    (2) Created CustomColors extension
    (3) Edit SideMenu pod to no longer try hiding status bar

---
+++ AniLog/AniLog/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  AniLog
+//
+//  Created by David Fang on 3/24/17.
+//  Copyright © 2017 RCDF. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ AniLog/AniLog/CustomColors.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColors.swift
+//  AniLog
+//
+//  Created by David Fang on 3/24/17.
+//  Copyright © 2017 RCDF. All rights reserved.
+//
+
+import UIKit
+
+let primaryColor: UIColor = UIColor.icebergBlue
+
+extension UIColor {
+    static let icebergBlue = UIColor(red: 212/255, green: 231/255, blue: 235, alpha: 1.0)
+}
