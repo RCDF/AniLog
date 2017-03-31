0c09406da1a58ea185eb6a2494b515a28a0cffa1
David Fang <i.davidfang@gmail.com>
2017-03-30 23:01:27 -0700

    Add cancel button to keyboard

--- AniLog/AniLog/TaskListViewController.swift
+++ AniLog/AniLog/TaskListViewController.swift
@@ -12,22 +12,27 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
-    
+
     var tempTasksList: [String] = [String]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
-        self.tableView.layer.borderWidth = 1.0
-        self.tableView.layer.borderColor = UIColor.icebergBlue.cgColor
-        self.textField.delegate = self
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.layer.borderWidth = 1.0
+        tableView.layer.borderColor = UIColor.icebergBlue.cgColor
+
+        textField.delegate = self
+        textField.inputAccessoryView = initializeKeyboard()
+
         let tapDismiss = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapDismiss.delegate = self
         view.addGestureRecognizer(tapDismiss)
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +40,6 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     }
 
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        
         if let view = touch.view {
             if view is UITableViewCell || view.superview is UITableViewCell || view.superview?.superview is UITableViewCell {
                 return false
@@ -44,8 +48,19 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
 
         return true
     }
-    
-    
+
+    func initializeKeyboard() -> UIToolbar {
+        let keyboardToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissKeyboard))
+
+        keyboardToolbar.barStyle = .default
+        keyboardToolbar.items = [closeButton]
+        keyboardToolbar.sizeToFit()
+
+        return keyboardToolbar
+    }
+
+
     // MARK: - TableView Delegate Functions
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -59,11 +74,11 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          let cell = tableView.dequeueReusableCell(withIdentifier: "taskListCell", for: indexPath) as! TaskListCell
         cell.taskName.text = tempTasksList[indexPath.row]
-        
+
         cell.preservesSuperviewLayoutMargins = false
         cell.separatorInset = UIEdgeInsets.zero
         cell.layoutMargins = UIEdgeInsets.zero
-        
+
         return cell
     }
 
@@ -71,44 +86,44 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
         print(tempTasksList[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    
+
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete", handler: deleteTask)
-        
+
         let editAction = UITableViewRowAction(style: .normal, title: "Edit", handler: editTask)
-        
+
         return [deleteAction, editAction]
     }
 
     // MARK: - Task Editting Handlers
-    
+
     func editTask(action: UITableViewRowAction, indexPath: IndexPath) {
         // Edit stuff
         tableView.reloadData()
     }
-    
+
     func deleteTask(action: UITableViewRowAction, indexPath: IndexPath) {
         tempTasksList.remove(at: indexPath.row)
         tableView.beginUpdates()
         tableView.deleteRows(at: [indexPath], with: .fade)
         tableView.endUpdates()
     }
-    
+
     // MARK: - TextField Functions
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         addTask(textField)
         return true
     }
-    
+
     func dismissKeyboard() {
         textField.resignFirstResponder()
     }
-    
+
     @IBAction func addTask(_ sender: Any) {
         if let text = textField.text {
             if (text != "") {
